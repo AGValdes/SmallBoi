a097c63a87c8dade174487971b0815a4f20876d2
Mike Falk <falk478@yahoo.com>
2021-03-16 11:45:42 -0700

    added timer still need to move to corner of the screen.

---
+++ Assets/Scripts/CountDownScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownScript : MonoBehaviour
+{
+    [SerializeField] private Text uiText;
+    [SerializeField] private float mainTimer;
+
+    private float timer;
+    private bool canCount = true;
+    private bool doOnce = false;
+
+    
+    void Start()
+    {
+        
+        
+        timer = mainTimer;
+        doOnce = true;
+    }
+
+
+    void Update()
+    {
+
+        if(doOnce)
+        {
+            timer += Time.deltaTime;
+            uiText.text = timer.ToString("f");
+        }
+
+        else if (timer <= 0.0f && !doOnce)
+        {
+            canCount = false;
+            doOnce = true;
+            uiText.text = "0.00";
+            timer = 0.0f;
+        }
+    }
+
+}
