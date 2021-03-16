1de3cc24cf03930e75a7ef9db7770e85f0ee0b1d
Overholtk <overholtkaya@gmail.com>
2021-03-16 10:35:57 -0700

    item spawn finished

--- Assets/Scripts/Environment/ItemAssets.cs
+++ Assets/Scripts/Environment/ItemAssets.cs
@@ -18,9 +18,7 @@ public class ItemAssets : MonoBehaviour
         //TEMPLATE: ItemWorld.SpawnItemWorld(new Vector2(4.5f, -0.04f), new Item { itemType = Item.ItemType.[ITEM TYPE HERE] });
 
         ItemWorld.SpawnItemWorld(new Vector2(-7.97f, 4.25f), new Item { itemType = Item.ItemType.serotoninCrumb });
-
-
-
+        ItemWorld.SpawnItemWorld(new Vector3(0.68f, 1.15f, -5.0f), new Item { itemType = Item.ItemType.star });
 
     }
 }
