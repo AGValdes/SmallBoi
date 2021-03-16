8a084249260a6bf3a2f37ae6dbf4e90227bc96e2
daviddicken <davidfromseattle@gmail.com>
2021-03-16 09:53:33 -0700

    starting day 2

--- Assets/Scripts/Movement/PlayerOne.cs
+++ Assets/Scripts/Movement/PlayerOne.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,20 +11,20 @@ public class PlayerOne : MonoBehaviour
 
     private Rigidbody2D rigidBody2D;
     private BoxCollider2D boxCollider2D;
-    [SerializeField] private LayerMask platformLayerMask;
+
     [SerializeField] private LayerMask playerLayerMask;
-    [SerializeField] private LayerMask enemyLayerMask;
+    [SerializeField] private LayerMask grounded;
 
 
     private float jumpVelocity = 0;
+
     private int direction;
+
     void Start()
     {
         animator = GetComponent<Animator>();
-
         rigidBody2D = transform.GetComponent<Rigidbody2D>();
         boxCollider2D = transform.GetComponent<BoxCollider2D>();
-       
     }
 
     void Update()
@@ -32,13 +33,15 @@ public class PlayerOne : MonoBehaviour
 
 
         //============= Key presses ======================
-
         if (Input.GetKey(KeyCode.DownArrow))
         {
             RaycastHit2D targetObj = GetThrowObj();
             float hor = 7f * direction;
             float vert = 7f;
-            targetObj.collider.attachedRigidbody.velocity = new Vector2(hor, vert);
+            if (targetObj)
+            {
+                targetObj.collider.attachedRigidbody.velocity = new Vector2(hor, vert);
+            }
         } 
 
         // may toggle speed run
@@ -61,9 +64,9 @@ public class PlayerOne : MonoBehaviour
             jumpVelocity = 5f;
             rigidBody2D.velocity = Vector2.up * jumpVelocity;
         }
+
     }
 
-    //================= Methods ====================
     /// <summary>
     /// Makes sure the player is not in the air.
     /// </summary>
@@ -71,9 +74,9 @@ public class PlayerOne : MonoBehaviour
     private bool IsGrounded()
     {
         RaycastHit2D raycastPlatform = Physics2D.BoxCast(boxCollider2D.bounds.center,
-            boxCollider2D.bounds.size, 0f, Vector2.down, .1f, platformLayerMask);
+            boxCollider2D.bounds.size, 0f, Vector2.down, .1f, grounded);
         RaycastHit2D raycastPlayer = Physics2D.BoxCast(boxCollider2D.bounds.center,
-        boxCollider2D.bounds.size, 0f, Vector2.down, .1f, playerLayerMask);
+        boxCollider2D.bounds.size, 0f, Vector2.down, .1f, grounded);
         //raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center,
         //boxCollider2D.bounds.size, 0f, Vector2.down, .1f, enemyLayerMask);
 
@@ -87,18 +90,18 @@ public class PlayerOne : MonoBehaviour
     /// <summary>
     /// Finds which edge collision occured on and moves child object with parent
     /// </summary>
-    /// <param name="collision">Collision2D</param>
+    /// <param name="collision">Collision2D</param>    
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.CompareTag("player 2") || collision.collider.CompareTag("moveable"))
+
+        RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center,
+            boxCollider2D.bounds.size, 0f, Vector2.up, .1f, playerLayerMask);
+        if (raycastHit2D)
         {
-            Collision2DSideType collisionSide = collision.GetContactSide();
-            if (collisionSide == Collision2DSideType.Top)
-            {
-                collision.collider.transform.SetParent(transform);
-            }
+            raycastHit2D.collider.transform.SetParent(transform);
         }
     }
+    
     /// <summary>
     /// Removes the connection between parent and child
     /// </summary>
@@ -107,6 +110,7 @@ public class PlayerOne : MonoBehaviour
     {
         collision2D.collider.transform.parent = null;
     }
+    
     /// <summary>
     /// Gets object on top of this object
     /// </summary>
@@ -118,99 +122,4 @@ public class PlayerOne : MonoBehaviour
 
         return raycastHit2D;
     }
-}// end of class
-
-
-//===================== Collision side stuff ==========================
-public enum Collision2DSideType
-{
-    None,
-    Left,
-    Right,
-    Top,
-    Bottom,
 }
-public static class Collision2DExtensions
-{
-    public static Collision2DSideType GetContactSide(Vector2 max, Vector2 center, Vector2 contact)
-    {
-        Collision2DSideType side = Collision2DSideType.None;
-        float diagonalAngle = Mathf.Atan2(max.y - center.y, max.x - center.x) * 180 / Mathf.PI;
-        float contactAngle = Mathf.Atan2(contact.y - center.y, contact.x - center.x) * 180 / Mathf.PI;
-        if (contactAngle < 0)
-        {
-            contactAngle = 360 + contactAngle;
-        }
-        if (diagonalAngle < 0)
-        {
-            diagonalAngle = 360 + diagonalAngle;
-        }
-        if (
-            ((contactAngle >= 360 - diagonalAngle) && (contactAngle <= 360)) ||
-            ((contactAngle <= diagonalAngle) && (contactAngle >= 0))
-        )
-        {
-            side = Collision2DSideType.Right;
-        }
-        else if (
-            ((contactAngle >= 180 - diagonalAngle) && (contactAngle <= 180)) ||
-            ((contactAngle >= 180) && (contactAngle <= 180 + diagonalAngle))
-        )
-        {
-            side = Collision2DSideType.Left;
-        }
-        else if (
-            ((contactAngle >= diagonalAngle) && (contactAngle <= 90)) ||
-            ((contactAngle >= 90) && (contactAngle <= 180 - diagonalAngle))
-        )
-        {
-            side = Collision2DSideType.Top;
-        }
-        else if (
-            ((contactAngle >= 180 + diagonalAngle) && (contactAngle <= 270)) ||
-            ((contactAngle >= 270) && (contactAngle <= 360 - diagonalAngle))
-        )
-        {
-            side = Collision2DSideType.Bottom;
-        }
-        return side.Opposite();
-    }
-    public static Collision2DSideType GetContactSide(this Collision2D collision)
-    {
-        Vector2 max = collision.collider.bounds.max;
-        Vector2 center = collision.collider.bounds.center;
-        Vector2 contact = collision.GetContact(0).point;
-        return GetContactSide(max, center, contact);
-    }
-}
-public static class Collision2DSideTypeExtensions
-{
-    public static Collision2DSideType Opposite(this Collision2DSideType sideType)
-    {
-        Collision2DSideType opposite;
-        if (sideType == Collision2DSideType.Left)
-        {
-            opposite = Collision2DSideType.Right;
-        }
-        else if (sideType == Collision2DSideType.Right)
-        {
-            opposite = Collision2DSideType.Left;
-        }
-        else if (sideType == Collision2DSideType.Top)
-        {
-            opposite = Collision2DSideType.Bottom;
-        }
-        else if (sideType == Collision2DSideType.Bottom)
-        {
-            opposite = Collision2DSideType.Top;
-        }
-        else
-        {
-            opposite = Collision2DSideType.None;
-        }
-        return opposite;
-    }
-
-}
-
-
