4fe7e83328eb235f0229114733f486eba342b5a5
daviddicken <davidfromseattle@gmail.com>
2021-03-15 17:50:57 -0700

    pushing character movement

--- Assets/Scripts/PlayerOne.cs
+++ Assets/Scripts/PlayerOne.cs
@@ -23,23 +23,23 @@ public class PlayerOne : MonoBehaviour
 
         rigidBody2D = transform.GetComponent<Rigidbody2D>();
         boxCollider2D = transform.GetComponent<BoxCollider2D>();
+       
     }
 
     void Update()
     {
         //animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
 
+
+        //============= Key presses ======================
+
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            RaycastHit2D targetObj = Throw();
-
-
-            jumpVelocity = 5f;
-            targetObj.rigidBody2D.velocity = Vector2.up * jumpVelocity;
-
-
-
-        }
+            RaycastHit2D targetObj = GetThrowObj();
+            float hor = 7f * direction;
+            float vert = 7f;
+            targetObj.collider.attachedRigidbody.velocity = new Vector2(hor, vert);
+        } 
 
         // may toggle speed run
         Vector3 move = new Vector3();
@@ -61,48 +61,62 @@ public class PlayerOne : MonoBehaviour
             jumpVelocity = 5f;
             rigidBody2D.velocity = Vector2.up * jumpVelocity;
         }
-
-        //if (!IsGrounded())
-        //{
-        //    move = new Vector3(direction, 0.0f, 0.0f);
-        //    this.transform.position = this.transform.position + ((move * Time.deltaTime) * speed);
-        //}
     }
+
+    //================= Methods ====================
+    /// <summary>
+    /// Makes sure the player is not int the air.
+    /// </summary>
+    /// <returns>bool</returns>
     private bool IsGrounded()
     {
-        RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center,
+        RaycastHit2D raycastPlatform = Physics2D.BoxCast(boxCollider2D.bounds.center,
             boxCollider2D.bounds.size, 0f, Vector2.down, .1f, platformLayerMask);
-        //raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center,
-        //boxCollider2D.bounds.size, 0f, Vector2.down, .1f, playerLayerMask);
+        RaycastHit2D raycastPlayer = Physics2D.BoxCast(boxCollider2D.bounds.center,
+        boxCollider2D.bounds.size, 0f, Vector2.down, .1f, playerLayerMask);
         //raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center,
         //boxCollider2D.bounds.size, 0f, Vector2.down, .1f, enemyLayerMask);
-        return raycastHit2D.collider != null;
+
+        if(raycastPlatform || raycastPlayer)
+        {
+            return true;
+        }
+        return false;
     }
 
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    if (collision.collider.CompareTag("player 2"))
-    //    {
-
-    //        Collision2DSideType collisionSide = collision.GetContactSide();
-    //        if (collisionSide == Collision2DSideType.Top)
-    //        {
-                
-    //        }
-    //    }
-    //}
-
-
-    private RaycastHit2D Throw()
+    /// <summary>
+    /// Finds which edge collision occured on and moves child object with parent
+    /// </summary>
+    /// <param name="collision">Collision2D</param>
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("player 2"))
+        {
+            Collision2DSideType collisionSide = collision.GetContactSide();
+            if (collisionSide == Collision2DSideType.Top)
+            {
+                collision.collider.transform.SetParent(transform);
+            }
+        }
+    }
+    /// <summary>
+    /// Removes the connection between parent and child
+    /// </summary>
+    /// <param name="collision2D"></param>
+    private void OnCollisionExit2D(Collision2D collision2D)
+    {
+        collision2D.collider.transform.parent = null;
+    }
+    /// <summary>
+    /// Gets object on top of this object
+    /// </summary>
+    /// <returns>RaycastHit2D</returns>
+    private RaycastHit2D GetThrowObj()
     {
         RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center,
             boxCollider2D.bounds.size, 0f, Vector2.up, .1f, playerLayerMask);
-        
-        if(raycastHit2D.collider != null)
-        {
-             return raycastHit2D;          
-        }
-        return null;
+
+        return raycastHit2D;
     }
 
 
--- Assets/Scripts/PlayerTwo.cs
+++ Assets/Scripts/PlayerTwo.cs
@@ -14,29 +14,49 @@ public class PlayerTwo : MonoBehaviour
     [SerializeField] private LayerMask platformLayerMask;
     [SerializeField] private LayerMask playerLayerMask;
     [SerializeField] private LayerMask enemyLayerMask;
+    [SerializeField] private Transform topSensor;
 
     private float jumpVelocity = 0;
 
+    private int direction;
     void Start()
     {
         animator = GetComponent<Animator>();
 
         rigidBody2D = transform.GetComponent<Rigidbody2D>();
         boxCollider2D = transform.GetComponent<BoxCollider2D>();
+
     }
 
     void Update()
     {
+        //animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
 
+        
+
+        //============= Key presses ======================
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            RaycastHit2D targetObj = GetThrowObj();
+            float hor = 7f * direction;
+            float vert = 7f;
+            targetObj.collider.attachedRigidbody.velocity = new Vector2(hor, vert);
+        }
+
+        // may toggle speed run
         Vector3 move = new Vector3();
         if (Input.GetKey(KeyCode.A))
         {
+            direction = -1;
             move = new Vector3(-1.0f, 0.0f, 0.0f);
         }
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
+            direction = 1;
             move = new Vector3(1.0f, 0.0f, 0.0f);
         }
+
         this.transform.position = this.transform.position + ((move * Time.deltaTime) * speed);
 
         if (Input.GetKeyDown(KeyCode.W) && IsGrounded())
@@ -46,15 +66,69 @@ public class PlayerTwo : MonoBehaviour
         }
     }
 
+    //================= Methods ====================
+    /// <summary>
+    /// Makes sure the player is not int the air.
+    /// </summary>
+    /// <returns>bool</returns>
     private bool IsGrounded()
     {
-        RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center,
+        RaycastHit2D raycastPlatform = Physics2D.BoxCast(boxCollider2D.bounds.center,
             boxCollider2D.bounds.size, 0f, Vector2.down, .1f, platformLayerMask);
-        //raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center,
-        //boxCollider2D.bounds.size, 0f, Vector2.down, .1f, playerLayerMask);
+        RaycastHit2D raycastPlayer = Physics2D.BoxCast(boxCollider2D.bounds.center,
+        boxCollider2D.bounds.size, 0f, Vector2.down, .1f, playerLayerMask);
         //raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center,
         //boxCollider2D.bounds.size, 0f, Vector2.down, .1f, enemyLayerMask);
-        return raycastHit2D.collider != null;
 
+        if (raycastPlatform || raycastPlayer)
+        {
+            return true;
+        }
+        return false;
     }
+
+    /// <summary>
+    /// Finds which edge collision occured on and moves child object with parent
+    /// </summary>
+    /// <param name="collision">Collision2D</param>
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("player 1"))
+        {
+            Collision2DSideType collisionSide = collision.GetContactSide();
+            Debug.Log(collisionSide.ToString());
+            if (collisionSide == Collision2DSideType.Top)
+            {
+                collision.collider.transform.SetParent(transform);
+            }
+        }
+    }
+    /// <summary>
+    /// Removes the connection between parent and child
+    /// </summary>
+    /// <param name="collision2D"></param>
+    private void OnCollisionExit2D(Collision2D collision2D)
+    {
+        collision2D.collider.transform.parent = null;
+    }
+    /// <summary>
+    /// Gets object on top of this object
+    /// </summary>
+    /// <returns>RaycastHit2D</returns>
+    private RaycastHit2D GetThrowObj()
+    {
+        RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center,
+            boxCollider2D.bounds.size, 0f, Vector2.up, .1f, playerLayerMask);
+
+        return raycastHit2D;
+    }
+
+
+
+
 }
+
+
+
+
+//===================== Collision side stuff ==========================
